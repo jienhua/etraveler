295b09a4ee335932f427d7dc922add9f304a5370
jienhua <jien8013@hotmail.com>
2016-05-24 15:31:29 -0700

    add traveler page UI

--- public/js/controllers/TravelerCtrl.js
+++ public/js/controllers/TravelerCtrl.js
@@ -2,9 +2,28 @@ angular.module('TravelerCtrl', [])
 	
 	.controller('TravelerController', ['$scope','Traveler',  function($scope, Traveler){
 
-		$scope.tagline = 'Nothing beats a pocket protector!';
+		$scope.currentStep = 1;
+		$scope.currentSubStep = 1;
+
 		Traveler.get()
 			.success(function(data){
-				$scope.forms = data;
+				$scope.forms = data.forms;
+				// $scope.SN = data.forms.SN;
 			});
+
+		$scope.next = function(){
+			var object = $scope.forms.steps[$scope.currentStep-1];
+			if(object.list.length >= $scope.currentSubStep+1){
+				$scope.currentSubStep += 1;
+			}else{
+				$scope.currentStep += 1;
+				$scope.currentSubStep = 1;
+			}
+		};
+
+		$scope.statusPage = function(page){
+			$scope.currentStep = page;
+			$scope.currentSubStep = 1;
+		}
+
 }])
