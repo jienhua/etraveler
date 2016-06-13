3e8fd5e94c476c00b68bd9a99a68f22bac3f260d
jienhua <jien8013@hotmail.com>
2016-06-13 16:11:07 -0700

    add password to generator and checkpoint json

--- public/js/controllers/FormGenCtrl.js
+++ public/js/controllers/FormGenCtrl.js
@@ -4,9 +4,15 @@ angular.module('FormGenCtrl.js', [])
 
 		$scope.submit = function(){
 			// console.log($scope.template);
+			var password = prompt('password');
+			if(password === 'ji394su3'){
 			Form.create($scope.template)
 				.success(data =>{
 					// do something
+					alert('add');
 				});
+			}else{
+				alert('go home son');
+			}
 		}
 	}]);
